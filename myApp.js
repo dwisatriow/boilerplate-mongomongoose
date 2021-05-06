9b426b483e2b21447d9de843a322308f3d95534a
dwisatriow <31796624+dwisatriow@users.noreply.github.com>
2021-05-07 03:19:54 +0700

    Update queryChain function to find specific document by favoriteFoods

--- myApp.js
+++ myApp.js
@@ -101,8 +101,14 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort("name")
+    .limit(2)
+    .select("-age")
+    .exec(function (err, data) {
+      if (err) console.error(err);
+      done(null, data);
+    });
 };
 
 /** **Well Done !!**
